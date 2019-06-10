f011b71cc04e264fa0d1e9be1fd4494584c57992
DragonYinghaiLi <37654394+DragonYinghaiLi@users.noreply.github.com>
2019-06-10 16:16:14 +0800

    修改后提交

---
+++ src/main/java/com/okami/apps/ej/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,22 @@
+package com.okami.apps.ej.service.impl;
+
+import com.okami.apps.ej.bean.Customer;
+import com.okami.apps.ej.bean.CustomerExample;
+import com.okami.apps.ej.dao.CustomerMapper;
+import com.okami.apps.ej.service.ICustomerService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements ICustomerService {
+ @Resource
+    private CustomerMapper customerMapper;
+
+    @Override
+    public List<Customer> findAll() {
+        CustomerExample example=new CustomerExample();
+        return customerMapper.selectByExample(example);
+    }
+}
