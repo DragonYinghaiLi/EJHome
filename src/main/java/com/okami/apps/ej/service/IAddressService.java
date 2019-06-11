ac168d188782e8e8716463c834ece907f325fc3e
lyl <123456>
2019-06-11 16:39:48 +0800

    6.11.2019

--- src/main/java/com/okami/apps/ej/service/IAddressService.java
+++ src/main/java/com/okami/apps/ej/service/IAddressService.java
@@ -1,4 +1,10 @@
 package com.okami.apps.ej.service;
 
+import com.okami.apps.ej.bean.Address;
+
+import java.util.List;
+
 public interface IAddressService {
+    List<Address> findAll();
+    void deleteById(long id) throws Exception;
 }
--- src/main/java/com/okami/apps/ej/service/impl/AddressServiceImpl.java
+++ src/main/java/com/okami/apps/ej/service/impl/AddressServiceImpl.java
@@ -1,6 +1,30 @@
 package com.okami.apps.ej.service.impl;
 
+import com.okami.apps.ej.bean.Address;
+import com.okami.apps.ej.bean.AddressExample;
+import com.okami.apps.ej.dao.AddressMapper;
 import com.okami.apps.ej.service.IAddressService;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
 public class AddressServiceImpl implements IAddressService {
+    @Resource
+    private AddressMapper addressMapper;
+    @Override
+    public List<Address> findAll(){
+        AddressExample example=new AddressExample();
+        return addressMapper.selectByExample(example);
+    }
+    @Override
+    public void deleteById(long id) throws Exception{
+        Address address=addressMapper.selectByPrimaryKey(id);
+        if (address==null){
+            throw new Exception("删除的地址内容不存在");
+        }else{
+            addressMapper.deleteByPrimaryKey(id);
+        }
+    }
 }
