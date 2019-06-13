f25fb525b4f6124a6b5f5a69f6cb35bf167289ec
lyl <123456>
2019-06-13 17:14:04 +0800

    6.13.2019

--- src/main/java/com/okami/apps/ej/web/controller/OrderLineController.java
+++ src/main/java/com/okami/apps/ej/web/controller/OrderLineController.java
@@ -14,28 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/order")
-public class OrderController {
-    @Autowired
-    private IOrderLineService orderlineService;
 
-    @GetMapping("findAll")
-    @ApiOperation("查询其他信息")
-    public Message findAll(){
-        List<OrderLine> list=orderlineService.findAll();
-        return MessageUtil.success("success",list);
-    }
+    @RestController
+    @RequestMapping("/order")
+    public class OrderLineController {
+        @Autowired
+        private IOrderLineService orderlineService;
 
-    @GetMapping("deleteById")
-    @ApiOperation("通过id删除其他信息")
-    public String deleteById(@ApiParam(value = "主键",required = true)@RequestParam("id")long id){
-        try{
-            orderlineService.deleteById(id);
-            return "删除成功";
-        }catch(Exception e){
-            e.printStackTrace();
-            return e.getMessage();
+        @GetMapping("findAll")
+        @ApiOperation("查询其他信息")
+        public Message findAll(){
+            List<OrderLine> list=orderlineService.findAll();
+            return MessageUtil.success("success",list);
+        }
+
+        @GetMapping("deleteById")
+        @ApiOperation("通过id删除其他信息")
+        public String deleteById(@ApiParam(value = "主键",required = true)@RequestParam("id")long id){
+            try{
+                orderlineService.deleteById(id);
+                return "删除成功";
+            }catch(Exception e){
+                e.printStackTrace();
+                return e.getMessage();
+            }
         }
     }
-}
+
+
